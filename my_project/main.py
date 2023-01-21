0f4c8055f9e6921d24160528408126fedda02817
haku <gentlebuuny@gmail.com>
2023-01-21 18:22:07 +0900

    feat: change message

--- my_project/main.py
+++ my_project/main.py
@@ -1,32 +1,56 @@
-from fastapi import FastAPI
+import random
+import string
 
+from fastapi import FastAPI
 
 app = FastAPI()
 
-import random
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-import string
-@app.get('/foo')
+
+@app.get("/foo")
 async def get_foo():
-    random_string = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(12))
-    return {'my_message': f'hello, foo world! {random_string}'}
-@app.get('/bar')
+    random_string = "".join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(12))
+    # 필독 🙋 🙋 🙋
+    # "my_message"는 main 브랜치 코드를 따라가고 (바꾸지 말고),
+    # Howard는 자기 이름으로 바꿀 것
+    return {"my_message": f"hello, Howard world! {random_string}"}
+
+
+@app.get("/bar")
 async def get_bar():
-    random_string = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(12))
-    return {'my_message': f'hello, bar world! {random_string}'}
-@app.get('/spam')
+    random_string = "".join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(12))
+    # 필독 🙋 🙋 🙋
+    # "my_message"는 main 브랜치 코드를 따라가고 (바꾸지 말고),
+    # Howard는 자기 이름으로 바꿀 것
+    return {"my_message": f"hello, Howard world! {random_string}"}
+
+
+@app.get("/spam")
 async def get_spam():
-    random_string = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(12))
-    return {'my_message': f'hello, spam world! {random_string}'}
-@app.get('/eggs')
+    random_string = "".join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(12))
+    # 필독 🙋 🙋 🙋
+    # "my_message"는 main 브랜치 코드를 따라가고 (바꾸지 말고),
+    # Howard는 자기 이름으로 바꿀 것
+    return {"my_message": f"hello, Howard world! {random_string}"}
+
+
+@app.get("/eggs")
 async def get_eggs():
-    random_string = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(12))
-    return {'my_message': f'hello, eggs world! {random_string}'}
-@app.get('/bacon')
+    random_string = "".join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(12))
+    # 필독 🙋 🙋 🙋
+    # "my_message"는 main 브랜치 코드를 따라가고 (바꾸지 말고),
+    # Howard는 자기 이름으로 바꿀 것
+    return {"my_message": f"hello, Howard world! {random_string}"}
+
+
+@app.get("/bacon")
 async def get_bacon():
-    random_string = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(12))
-    return {'my_message': f'hello, bacon world! {random_string}'}
+    random_string = "".join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(12))
+    # 필독 🙋 🙋 🙋
+    # "my_message"는 main 브랜치 코드를 따라가고 (바꾸지 말고),
+    # Howard는 자기 이름으로 바꿀 것
+    return {"my_message": f"hello, Howard world! {random_string}"}
